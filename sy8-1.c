42fb74d645f7cbb0fa4fb52963b599e5cafc9d4e
TianXing Xue <76930817+veritas0518@users.noreply.github.com>
2021-06-02 18:16:48 +0800

    Find the minimum spanning tree by Prima algorithm
    【问题描述】
    
    实验8 最小生成树
    
    1．实验目的
    
    （1）复习图的存储方法和图的遍历方法。
    
    （2）进一步掌握图的非线性特点、递归特点和动态特性。
    
    （3）掌握最小生成树的求解算法。
    
    2．实验内容
    
    （1）用Prim算法求最小生成树。
    
    （2）输入网的二维矩阵，输出最小生成树。 
    
    
    
    【输入形式】
    
    第一行输入：顶点数和边数
    
    第二行输入：顶点的编号
    
    第三行开始输入：连接每条边（弧）的两个顶点编号及权值
    
    再输入起始点的编号
    
    
    
    【输出形式】
    
    输出最小生成树的对应顶点序列
    
    输出最小生成树的权值之和
    
    
    
    【样例输入1】
    
    6 8
    
    1 2 3 4 5 6
    
    1 2 6
    
    1 3 8
    
    2 4 12
    
    2 5 14
    
    3 4 16
    
    3 5 17
    
    4 5 15
    
    5 6 5
    
    1
    
    
    
    【样例输出1】
    
    (1,2)
    
    (1,3)
    
    (2,4)
    
    (2,5)
    
    (5,6)
    
    45
    
    
    
    
    
    【样例输入2】
    
    9 16
    
    1 2 3 4 5 6 7 8 9
    
    1 2 5
    
    1 3 13
    
    1 4 12
    
    1 5 10
    
    1 6 8
    
    1 7 6
    
    1 8 2
    
    1 9 5
    
    2 3 3
    
    3 4 9
    
    4 5 11
    
    5 6 9
    
    6 7 6
    
    7 8 7
    
    8 9 4
    
    9 2 1
    
    1
    
    
    
    【样例输出2】
    
    (1,8)
    
    (8,9)
    
    (9,2)
    
    (2,3)
    
    (1,7)
    
    (7,6)
    
    (3,4)
    
    (6,5)
    
    40

---
+++ sy8-1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#define big 9998
+#define max 40
+void prim(int g[][max],int n)		
+{
+ int lc[max],ct[max];
+ int i,j,k,min,sum=0;
+ for(i=2;i<=n;i++){ 				
+ 	lc[i]=g[1][i]; 		
+ 	ct[i]=1; 					
+ }
+ lc[1]=0; 					
+ for(i=2;i<=n;i++){ 					
+	 min=big;
+ 	k=0;
+ 	for(j=2;j<=n;j++)					
+ 		if((lc[j]<min)&&(lc[j]!=0)){
+     	 	min=lc[j];
+ 			k=j;
+     	}
+     sum+=min;
+	 printf("(%d,%d)",ct[k],k);
+ 	 lc[k]=0;					 
+ 	 for(j=2;j<=n;j++) 			
+     if(g[k][j]<lc[j]){ 
+     	lc[j]=g[k][j];
+        ct[j]=k;
+     }
+ 	printf("\n");
+ }
+ printf("%d",sum);
+} 
+int adjg(int g[][max]) 		
+{
+ 	int n,e,i,j,k,v1,v2,weight,x[max];
+ 	scanf("%d %d",&n,&e);
+ 	for(i=1;i<=n;i++)
+ 		scanf("%d",&x[n]);
+ 	for(i=1;i<=n;i++) 
+ 		for(j=1;j<=n;j++)
+ 			g[i][j]=big; 		
+ 		for(k=1;k<=e;k++){
+ 			scanf("%d %d %d",&v1,&v2,&weight);
+ 			g[v1][v2]=weight;
+ 			g[v2][v1]=weight;
+   		}
+ 	return(n);
+}
+int main()
+{
+ 	int g[max][max],n;
+ 	n=adjg(g);
+ 	prim(g,n);
+ 	return 0;
+}
+
+
